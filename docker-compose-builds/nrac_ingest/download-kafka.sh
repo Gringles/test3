8c6e79caa3d12497a80c8287c21e248dab657528
Gringles <42545430+Gringles@users.noreply.github.com>
2018-08-20 21:06:00 +0100

    new

---
+++ docker-compose-builds/nrac_ingest/download-kafka.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -e
+
+path="kafka/${KAFKA_VERSION}/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz"
+url="http://archive.apache.org/dist/${path}"
+
+if [[ -z "$url" ]]; then
+	echo "Unable to determine mirror for downloading Kafka, the service may be down"
+	exit 1
+fi
+
+wget -q "${url}" -O "/tmp/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz"
